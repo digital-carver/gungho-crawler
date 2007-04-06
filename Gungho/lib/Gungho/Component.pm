5189ae1c3539bd3209ec34dfa38b20bc17bbf1b8
lestrrat <lestrrat@be909cde-6b2d-0410-a0fd-a1076e6a2be7>
2007-04-06 06:37:56 +0000

    import

---
+++ Gungho/lib/Gungho/Component.pm
@@ -0,0 +1,58 @@
+# $Id$
+#
+# Copyright (c) 2007 Daisuke Maki <daisuke@endeworks.jp>
+# All rights reserved.
+
+package Gungho::Component;
+use strict;
+use base qw(Class::Accessor::Fast);
+use Class::C3;
+INIT { Class::C3::initialize() }
+
+__PACKAGE__->mk_accessors($_) for qw(config);
+
+sub new
+{
+    my $class  = shift;
+    my $config = shift;
+
+    my $self = bless {}, $class;
+    $self->config($config);
+    $self->setup();
+
+    return $self;
+}
+
+sub setup {}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Gungho::Component - Base Class For Various Gungho Components
+
+=head1 SYNOPSIS
+
+  package Gungho::Something;
+  use base qw(Gungho::Component);
+
+=head1 MMETHODS
+
+=head2 new(\%config)
+
+Creates a new component instance. Takes a config hashref. 
+
+=head2 setup()
+
+Sets up the components. Use it like this in your component:
+
+  sub setup
+  {
+     my $self = shift;
+     # do custom setup
+     $self->next::method(@_);
+  }
+
+=cut
---
+++ Gungho/lib/Gungho/Engine.pm
@@ -0,0 +1,37 @@
+# $Id$
+#
+# Copyright (c) 2007 Daisuke Maki <daisuke@endeworks.jp>
+# All rights reserved.
+
+package Gungho::Engine;
+use strict;
+use base qw(Gungho::Component);
+
+sub run {}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Gungho::Engine - Base Class For Gungho Engine
+
+=head1 SYNOPSIS
+
+  package Gungho::Engine::SomeEngine;
+  use strict;
+  use base qw(Gungho::Engine);
+
+  sub run
+  {
+     ....
+  }
+
+=head1 METHODS
+
+=head2 run()
+
+Starts the engine. The exact behavior differs between each engine
+
+=cut
