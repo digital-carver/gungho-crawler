5bd9169a4dd566f83a6001ec69ecc5c3bb766659
lestrrat <lestrrat@be909cde-6b2d-0410-a0fd-a1076e6a2be7>
2007-05-07 10:13:36 +0000

    FileWrite::Simple was too simple to have inheritance

--- Gungho/lib/Gungho/Handler/FileWriter/Simple.pm
+++ Gungho/lib/Gungho/Handler/FileWriter/Simple.pm
@@ -6,7 +6,7 @@
 package Gungho::Handler::FileWriter::Simple;
 use strict;
 use warnings;
-use base qw(Gungho::Handler::FileWriter);
+use base qw(Gungho::Handler);
 use File::Spec;
 use Path::Class();
 use URI::Escape qw(uri_escape);
@@ -33,4 +33,41 @@ sub path_to
     return $self->dir->file( uri_escape( $res->uri ) );
 }
 
+sub handle_response
+{
+    my ($self, $c, $req, $res) = @_;
+
+    my $file = $self->path_to($req, $res);
+    my $fh   = $file->openw() or die;
+
+    $c->log->debug("Writing " . $req->uri . " to $file");
+
+    $fh->print($res->content);
+    $fh->close;
+}
+
 1;
+
+__END__
+
+=head1 NAME
+
+Gungho::Handler::FileWriter::Simple - Write Out Fetched Contents To File
+
+=head1 DESCRIPTION
+
+This is a simple, dumb module mostly for demonstration purposes. It just
+writes out fetched contents to a single location in your file system.
+
+In real life, you probably want to hash them to different locations, and
+put better names to the files.
+
+=head1 METHODS
+
+=head2 setup
+
+=head2 path_to
+
+=head2 handle_response
+
+=cut
