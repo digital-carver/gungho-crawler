757ae65e17208c7bd8e376d8ae97471d31d6aa69
lestrrat <lestrrat@be909cde-6b2d-0410-a0fd-a1076e6a2be7>
2007-10-14 00:39:41 +0000

    Make things work with Cache::Memcached::Managed, allow configurable expiration

--- Gungho/lib/Gungho/Component/RobotRules/Storage/Cache.pm
+++ Gungho/lib/Gungho/Component/RobotRules/Storage/Cache.pm
@@ -8,15 +8,19 @@ use strict;
 use warnings;
 use base qw(Gungho::Component::RobotRules::Storage);
 
+__PACKAGE__->mk_accessors($_) for qw(expiration);
+
 sub setup
 {
     my $self = shift;
     my $c    = shift;
     my %config = %{ $self->{config} };
     my $module = delete $config{module} || 'Cache::Memcached';
+    my $expiration = delete $config{expiration} || 86400  * 7;
 
     Class::Inspector->loaded($module) or $module->require or die;
     $self->storage( $module->new(%config) );
+    $self->expiration( $expiration );
     $self->next::method(@_);
 }
 
@@ -41,7 +45,17 @@ sub put_rule
 
     my $uri = $request->original_uri;
     $c->log->debug("Saving robot rules for $uri");
-    $self->storage->set( $uri->host_port, $rule, 86400 * 7 );
+
+    # Cache:::Memcached::Managed is a bad boy and breaks API compatibility
+    # with the rest of the Cache::* modules
+    my @args;
+    my $storage = $self->storage;
+    if ($storage->isa('Cache::Memcached::Managed')) {
+        @args = (id => $uri->host_port, key => 'robot_rules', value => $rule, expiration => $self->expiration);
+    } else {
+        @args = ($uri->host_port, $rule, $self->expiration);
+    }
+    $self->storage->set( @args );
 }
 
 1;
@@ -52,6 +66,15 @@ __END__
 
 Gungho::Component::RobotRules::Storage::Cache - Cache Storage For RobotRules
 
+=head1 SYNOPSIS
+
+  robotrules:
+    cache:
+      module: 'Cache::Memcached'
+      expiration: 86400
+      servers:
+        - 127.0.0.1:11211
+
 =head1 METHODS
 
 =head2 setup
@@ -61,6 +84,3 @@ Gungho::Component::RobotRules::Storage::Cache - Cache Storage For RobotRules
 =head2 put_rule
 
 =cut
-
-
-1;
